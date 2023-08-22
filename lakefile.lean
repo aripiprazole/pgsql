ae3ee64f1ba629dfef7895c818db35e9b59501ab
Sofia Rodrigues <sofia@algebraic.dev>
2023-08-22 00:50:28 -0300

    feat: remove weird behavior with include

--- lakefile.lean
+++ lakefile.lean
@@ -18,7 +18,7 @@ lean_exe «pgsql» {
 target ffi.o pkg : FilePath := do
   let oFile := pkg.buildDir / "pgsql" / "ffi.o"
   let srcJob ← inputFile <| pkg.dir / "pgsql" / "ffi.cpp"
-  let flags := #["-I", (← getLeanIncludeDir).toString, "-fPIC", "-I", "/usr/include/postgresql"]
+  let flags := #["-I", (← getLeanIncludeDir).toString, "-fPIC"]
   buildO "pgsql.cpp" oFile srcJob flags "c++"
 
 extern_lib libleanpgsql pkg := do
