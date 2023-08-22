676cee8d2da16605b2b74721d5ea8bc784017e67
Sofia Rodrigues <sofia@algebraic.dev>
2023-08-22 20:29:12 -0300

    fix: add mutex to make things right

--- Pgsql/FFI.lean
+++ Pgsql/FFI.lean
@@ -1,10 +1,19 @@
+import Init.System.Mutex
+
 namespace Pgsql
+
+open IO
+
 
 opaque Pgsql.Nonempty : NonemptyType
 
-def Connection : Type := Pgsql.Nonempty.type
+def RawConn : Type := Pgsql.Nonempty.type
 
 def Cursor : Type := Pgsql.Nonempty.type
+
+structure Connection where
+  conn : RawConn
+  mutex : BaseMutex
 
 inductive Result where
   | ok
@@ -17,10 +26,18 @@ opaque initPgsql : IO Unit
 builtin_initialize initPgsql
 
 @[extern "lean_pgsql_new"]
-opaque connect : String → IO Connection
+opaque connectRaw : String → IO RawConn
 
 @[extern "lean_pgsql_exec"]
-opaque execCursor : (s: @& Connection) → String → Array String → IO Result
+opaque execCursorRaw : (s: @& RawConn) → String → Array String → IO Result
+
+def connect (s: String) : IO Connection := do pure { conn := (← connectRaw s), mutex := ← BaseMutex.new }
+
+def execCursor (s: Connection) (str: String) (a: Array String) : IO Result := do
+  s.mutex.lock
+  let res ← execCursorRaw s.conn str a
+  s.mutex.unlock
+  pure res
 
 @[extern "lean_pgsql_cursor_fields"]
 opaque Cursor.fields : (s: @& Cursor) → USize
